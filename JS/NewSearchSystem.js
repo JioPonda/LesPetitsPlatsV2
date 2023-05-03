cd7ddbed73d33dbad493b6db1c4adc1d9bb1266c
Jioponda <crazyponda@outlook.fr>
2023-05-03 17:52:43 +0200

    affichage des recettes en fonction de la recherche : 75%

--- JS/NewSearchSystem.js
+++ JS/NewSearchSystem.js
@@ -17,18 +17,19 @@ function searchAlgoV1() {
   const searchBarUstensiles = document.getElementById("ustensiles");
   const recipesContainer = document.querySelector(".all-card-container");
   const divCard = document.querySelectorAll(".div-card");
-  const divTitle = document.querySelectorAll(".card-title");
+  const cardTitle = document.querySelectorAll(".card-title");
+  const cardIngretient = document.querySelectorAll(".span-ingredient");
+  const cardDescription = document.querySelectorAll(".card-description");
   const errorMessage = document.querySelector("#error-search");
   /****  ALGORYTHME DE RECHERCHE V1 ****/
 
-  /** Recherche par noms des plats **/
-
   searchBar.addEventListener("keyup", function () {
+    /****/
+    /** Recherche par noms des plats **/
     const searchDish = searchBar.value.toLowerCase();
-    if (searchDish.length > 3) {
+    if (searchDish.length >= 3) {
       getRecipes().then((data) => {
         const recipesList = data.recipes;
-
         const filteredRecipes = recipesList.filter((recipe) => {
           return recipe.name.toLowerCase().includes(searchDish);
         });
@@ -36,7 +37,7 @@ function searchAlgoV1() {
         console.log(filteredRecipes);
 
         divCard.forEach((card, index) => {
-          const title = divTitle[index].textContent.toLowerCase();
+          const title = cardTitle[index].textContent.toLowerCase();
           if (!title.includes(searchDish)) {
             card.classList.add("hide-card");
             card.classList.remove("show-card");
@@ -47,16 +48,57 @@ function searchAlgoV1() {
         });
       });
     }
-    if (searchDish == "") {
+    if (searchDish.length < 3) {
       divCard.forEach((card) => {
         card.classList.add("show-card");
         card.classList.remove("hide-card");
       });
     }
+    /****/
+    /** recherche par ingrédients **/
+    getRecipes().then((data) => {
+      const ingredientsList = data.recipes;
+      const filteredIngredients = ingredientsList.filter((recipe) => {
+        return recipe.ingredients.some((ingredient) =>
+          ingredient.ingredient.toLowerCase().includes(searchDish)
+        );
+      });
+      console.log(filteredIngredients);
+
+      divCard.forEach((card, index) => {
+        const title = cardTitle[index].textContent.toLowerCase();
+        if (!title.includes(searchDish)) {
+          card.classList.add("hide-card");
+          card.classList.remove("show-card");
+        } else {
+          card.classList.add("show-card");
+          card.classList.remove("hide-card");
+        }
+      });
+    });
+    /****/
+    /** Recherche par appliance **/
+    getRecipes().then((data) => {
+      const recipesList = data.recipes;
+      const filteredRecipes = recipesList.filter((recipe) => {
+        return recipe.appliance.toLowerCase().includes(searchDish);
+      });
+      console.log(filteredRecipes);
+    });
+    /****/
+    /** Recherche par ustensils **/
+    getRecipes().then((data) => {
+      const recipesList = data.recipes;
+      const filteredRecipes = recipesList.filter((recipe) => {
+        return recipe.ustensils.some((ustensil) => {
+          return ustensil.toLowerCase().includes(searchDish);
+        });
+      });
+      console.log(filteredRecipes);
+    });
   });
 
-  /** recherche par ingrédients **/
-
+  /** recherche des ingrédients **/
   searchBarIngredient.addEventListener("keyup", function () {
     const searchIngredient = searchBarIngredient.value.toLowerCase();
 
